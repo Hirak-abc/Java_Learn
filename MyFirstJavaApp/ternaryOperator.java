a68f5496b2d55e062302091e2fe0ea6d29f9deb4
Hirak Nath <hiraknath816@gmail.com>
2025-06-01 20:34:42 +0530

    Add files via upload

---
+++ MyFirstJavaApp/ternaryOperator.java
@@ -0,0 +1,18 @@
+public class ternaryOperator {
+
+    public static void main(String[] args) {
+        
+        //ternary operator ? = Return 1 of 2 values if a condition is true
+
+        //variable = (condition) ? ifTrue : ifFalse;
+
+
+        int score = 70;
+
+        String passOrfail  = (score>=60) ? "PASS" : "FAIL";//The question mark generally makes a simple one liner if else statement
+
+        System.out.println(passOrfail);
+    }
+    
+    
+}
